01c8c75ea3bd7f82e53a032daf8fe686cdf8e23e
Naman Sikarwar <namansikarwar37@gmail.com>
2026-08-25 20:47:56 +0530

    new

---
+++ lab.js
@@ -0,0 +1,49 @@
+let name = "Naman";
+console.log(name);
+
+
+const pi = 3.14;
+console.log(pi);
+
+let firstName = "Naman";
+let lastName = "Sikarwar";
+let age = 18;
+
+age = 19;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+let name = "Naman";
+let name = "Abhay";
+
+console.log(name);
+
+const age = 18;
+
+age = 19;
+
+console.log(age);
+
+
+//valid
+let firstName;
+let age;
+let student_1;
+let $price;
+let totalMarks;
+
+
+//invalid
+let 1name;
+let first-name;
+let let;
+
+
+let name = "naman";
+let age = 19;
+
+console.log(typeof);
+
+
