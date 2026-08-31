d3da75b46638eaf720635f6ef460d46de9110389
Naman Sikarwar <namansikarwar37@gmail.com>
2026-08-31 12:32:14 +0530

    new

--- lab.js
+++ lab.js
@@ -1,49 +1,92 @@
-let name = "Naman";
-console.log(name);
+// let name = "Naman";
+// console.log(name);
 
 
-const pi = 3.14;
-console.log(pi);
+// const pi = 3.14;
+// console.log(pi);
 
-let firstName = "Naman";
-let lastName = "Sikarwar";
-let age = 18;
+// let firstName = "Naman";
+// let lastName = "Sikarwar";
+// let age = 18;
 
-age = 19;
+// age = 19;
 
-console.log(firstName);
-console.log(lastName);
-console.log(age);
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
 
-let name = "Naman";
-let name = "Abhay";
+// // let name = "Naman";
+// let name = "Abhay";
 
-console.log(name);
+// console.log(name);
 
-const age = 18;
+// const age = 18;
 
-age = 19;
+// age = 19;
 
-console.log(age);
-
-
-//valid
-let firstName;
-let age;
-let student_1;
-let $price;
-let totalMarks;
+// console.log(age);
 
 
-//invalid
-let 1name;
-let first-name;
-let let;
+// //valid
+// let firstName;
+// let age;
+// let student_1;
+// let $price;
+// let totalMarks;
 
+
+// //invalid
+// let 1name;
+// let first-name;
+// let let;
+
+
+// let name = "naman";
+// let age = 19;
+
+// console.log(typeof(name));
 
 let name = "naman";
 let age = 19;
+let isStudent = true;
 
-console.log(typeof);
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isStudent));
+
+let product = {
+  name : "Laptop",
+  price : 50000,
+  isStock : true
+}
+
+console.log(product)
+
+let colours = ["red", "blue", "yellow", "green"];
+
+console.log(colours[0]);
+console.log(colours[3]);
+
+let a = null;
+let b;
+
+console.log(a);
+console.log(b);
+
+let str = '150';
+
+let num1 = Number(str);
+let num2 = Number(parseInt);
+let num3 = +str;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+
+
+
+
+
 
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,7 @@
-age = 19;
+let name = "naman";
+// let age = 19;
+// let isStudent = true;
+
+// console.log(typeof(name));
+// console.log(typeof(age));
+// console.log(typeof(isStudent));
